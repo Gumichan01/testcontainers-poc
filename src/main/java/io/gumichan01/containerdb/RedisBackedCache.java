c658e1217ef0a9accaa5adf50d828257f6af0f06
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-11-10 15:26:53 +0100

    Fix warning

--- src/main/java/io/gumichan01/containerdb/RedisBackedCache.java
+++ src/main/java/io/gumichan01/containerdb/RedisBackedCache.java
@@ -2,24 +2,17 @@ package io.gumichan01.containerdb;
 
 import com.google.gson.Gson;
 import redis.clients.jedis.Jedis;
-import sun.misc.Cache;
 
 import java.util.Optional;
 
 /**
- * An implementation of {@link Cache} that stores data in Redis.
+ * An implementation of a cache that stores data in Redis.
  */
 public final class RedisBackedCache {
 
     private final Jedis jedis;
     private final String cacheName;
     private final Gson gson;
-
-    public RedisBackedCache(Jedis jedis, String cacheName) {
-        this.jedis = jedis;
-        this.cacheName = cacheName;
-        this.gson = new Gson();
-    }
 
     public RedisBackedCache(String address, Integer port) {
         this.jedis = new Jedis(address, port);
