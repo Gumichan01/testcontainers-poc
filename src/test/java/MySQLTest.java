0e738b8f0f50f658ad16af5b9278d9a5a505cdcf
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-11-11 11:35:38 +0100

    Use JDBC to execute query

--- src/test/java/MySQLTest.java
+++ src/test/java/MySQLTest.java
@@ -1,10 +1,10 @@
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.containers.MySQLContainer;
 
 import javax.sql.DataSource;
@@ -15,13 +15,13 @@ public class MySQLTest {
 
     private static Logger logger = LoggerFactory.getLogger(MySQLTest.class);
     private static DataSource dataSource;
-    @ClassRule
-    public static MySQLContainer mysql = new MySQLContainer("mysql:8.0.18");
+    private static MySQLContainer mysql;
 
     @BeforeClass
     public static void init() throws InterruptedException {
         //You can also use the GenericContainer for arbitrary containers
         //But there are convenient classes for common databases.
+        mysql = new MySQLContainer("mysql:8.0.18");
         mysql.start();
         logger.info("address - {}", mysql.getContainerIpAddress());
         logger.info("user - {}", mysql.getUsername());
@@ -43,6 +43,9 @@ public class MySQLTest {
     public void testTableCreation() {
         //use the database
         logger.info("OK");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("CREATE TABLE BAR (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(200));");
         assertTrue(true);
     }
 }
